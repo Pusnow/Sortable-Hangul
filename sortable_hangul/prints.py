40d3e6caf97c299ef8740db196128b77f5b3a6d9
Wonsup Yoon <pusnow@yonsei.ac.kr>
2017-05-04 15:13:33 +0900

    print code only for Hangul

--- sortable_hangul/prints.py
+++ sortable_hangul/prints.py
@@ -2,13 +2,16 @@ def pprint(ch: str) -> None:
 
     code_block = (ord(ch) >> 15) & 1
 
-    if code_block:
-        FINAL = (ord(ch) >> 7) & 0b11111111
-        BANGJEOM = (ord(ch) >> 3) & 0b11
-        TYPE = ord(ch) & 0b111
-        print("FINAL-TYPE:", hex(FINAL), hex(BANGJEOM), hex(TYPE))
+    if ord(ch) >= 0xF0000 and ord(ch) < 0x100000:
+        if code_block:
+            FINAL = (ord(ch) >> 7) & 0b11111111
+            BANGJEOM = (ord(ch) >> 3) & 0b11
+            TYPE = ord(ch) & 0b111
+            print("FINAL-TYPE:", hex(FINAL), hex(BANGJEOM), hex(TYPE))
 
+        else:
+            INITIAL = (ord(ch) >> 7) & 0b11111111
+            PEAK = ord(ch) & 0b1111111
+            print("INITIAL-PEAK:", hex(INITIAL), hex(PEAK))
     else:
-        INITIAL = (ord(ch) >> 7) & 0b11111111
-        PEAK = ord(ch) & 0b1111111
-        print("INITIAL-PEAK:", hex(INITIAL), hex(PEAK))
+        print(ch)
